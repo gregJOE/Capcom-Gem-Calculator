a695be1782582f2faf0df2cc32a3d114317603d4
root <root@ubuntu.ubuntu-domain>
2012-06-29 15:49:03 -0400

    Completed Assist and Vitality Gem Effects. Still a few bugs with Attack gems. Find a way to deal with damage numbers with separators like x. Should be a function already written that deals with the x scenario, but it hasn't been tested yet

--- src/js/gemEffects.js
+++ src/js/gemEffects.js
@@ -42,50 +42,13 @@ $('#gems3').live('change', function(){
 
 function calculateGemEffects(gemEffect)
 {
-	if (gemEffect == "immenseLvl1" || gemEffect == "immenseLvl2" || gemEffect == "immenseLvl3")
-	{
-		/* recalculate Damage values */
-		recalculateDamageGems();
-		console.log(gemEffect);
-		if (gemEffect == "immenseLvl2")
-		{
-			/* reduce speed here */
-                        calculateSpeedEffects(-10);
-		}
-		/* check the other gems to see if they're damage gems as well */
-	}
-	else if (gemEffect == "ironWallLvl1" || gemEffect == "ironWallLvl2" || gemEffect == "ironWallLvl3" || gemEffect == "fortLvl1" || gemEffect == "fortLvl2")
-	{
-		console.log("hi");
-		/* health temporarily increases? */
-		calculateDefenseGems(gemEffect);
-	}
-	else if (gemEffect == "divineSpeedLvl1" || gemEffect == "divineSpeedLvl2" || gemEffect == "divineSpeedLvl3")
-	{
-		recalculateSpeedGems();
-	}
-	else if (gemEffect == "onslaughtLvl1" || gemEffect == "onslaughtLvl2" || gemEffect == "profLvl1" || gemEffect == "profLvl3")
-	{
-		calculateCrossGaugeGems(gemEffect);
-	}
-	else if (gemEffect == "lifeLvl1" || gemEffect == "harmLvl1")
-	{
-		calculateVitalityGems(gemEffect);
-	}
-	else if (gemEffect == "easyInput" || gemEffect == "superEasyInput" == gemEffect == "cancel" || gemEffect == "autoThrow" || gemEffect == "autoBlock")
-	{
-		calculateAssistGems(gemEffect);
-	}
-	else
-	{	/* if its "none", check if the damage column was changed. For now, assume it was */
-		if (gemEffect == "none")
-		{
-			recalculateDamageGems();
-			recalculateDefenseGems();
-			recalculateSpeedGems();
-		}
-		/* calculate other gems effects */
-	}
+	resetValuesWhileKeepingGemConfig();
+		
+	recalculateDamageGems();
+	recalculateDefenseGems();
+	recalculateSpeedGems();
+	recalculateAssistGems();
+	recalculateVitalityGems();		
 	return;	
 }
 
@@ -117,6 +80,8 @@ function calculateDefenseGems(gemTitle)
         {
                 health = (health * (0.20)) + health;
 		$('.health').css("color", "green");
+
+		calculateDamageEffects(-10);
         }
 	else if (gemTitle == "ironWallLvl3")
         {
@@ -152,6 +117,7 @@ function calculateSpeedGems(gemTitle)
 	{
 		calculateSpeedEffects(15);
 		/* function here to reduce defense numbers by 8% */
+		calculateDefenseEffects(-8);
 	}
 	else if (gemTitle == "divineSpeedLvl3")
 	{
@@ -240,7 +206,7 @@ function calculateCrossGaugeGems(gemTitle)
 			}
 			
 			/* reduce speed by 10% */
-			
+			calculateSpeedEffects(-10);
 			/**** NOTE, FIGURE OUT A WAY TO MEASURE AMOUNT OF EX BAR USED BY CERTAIN MOVES *****/
 			/* take the numbers of an an entire string, and put it back into the original string
 			else if(($(this).is(":contains(',')" )))
@@ -309,13 +275,51 @@ function calculateCrossGaugeGems(gemTitle)
 /* this function just writes HTML code */
 function calculateVitalityGems(gemTitle)
 {
+	console.log("Vitality");
+
+	if (gemTitle == "lifeForceLvl1")
+	{
+		$("#additionalEffects").append("<li> Gradually restore 80 points of HP for 5 seconds </li>");
+	}
 	
+	else if (gemTitle == "harmonizeLvl1")
+	{
+		$("#additionalEffects").append("<li> Instally restore 80 points of HP </li>");
+	}	
 }
 
 /* this function just writes HTML code */
 function calculateAssistGems(gemTitle)
 {
+	console.log("Assist!");
+	/* might cause a bug where strings may be written over again */
+	if (gemTitle == "easyInput")
+	{
+		calculateDamageEffects(-10);
+		$("#additionalEffects").append("<li> Special Moves become easier to do </li>");
+	}
+	
+	else if (gemTitle == "superEasyInput")
+        {
+		calculateDamageEffects(-10)
+                $("#additionalEffects").append("<li>Special Moves become very easy to do </li>");
+        }
+	
+	else if (gemTitle == "cancelAssist")
+        {
+		calculateDamageEffects(-10)
+		$("#additionalEffects").append("<li>Special Move cancels become easy to do</li>");
+        }
 
+	else if (gemTitle == "autoThrowEscape")
+        {
+		$("#additionalEffects").append("<li>Escape from throws automatically</li> <li> Cross Gauge decreases by 1/2 block when activated </li>");
+        }
+	
+	else if (gemTitle == "autoBlock")
+        {
+		$("#additionalEffects").append("<li>Block Attacks automatically</li> <li> Cross Gauge decreases by 1 block when activated </li>");
+        }
 }
 
 function calculateDamage(gemTitle)
@@ -343,6 +347,7 @@ function calculateDamage(gemTitle)
 				newString = newString.substring(0,newString.length-2);			
 				$(this).html(newString);	
 			}
+			
 			else
 			{
 				var value = parseFloat($(this).html());
@@ -385,8 +390,10 @@ function calculateDamage(gemTitle)
 			}
 			$(this).css('color', 'green');
 
-			/* reduce speed here */
 		})
+		
+		/* reduce speed here */
+		calculateSpeedEffects(-10);
 	}
 	
 	else if (gemTitle == "immenseLvl3"){
@@ -425,8 +432,6 @@ function calculateDamage(gemTitle)
 
 function recalculateDamageGems()
 {
-	resetDamageValues();
-		
 	calculateDamage($("#gems1").val());
 	calculateDamage($("#gems2").val());
 	calculateDamage($("#gems3").val());
@@ -436,8 +441,6 @@ function recalculateDamageGems()
 
 function recalculateDefenseGems()
 {
-        resetDefenseValues();
-
         calculateDefenseGems($("#gems1").val());
 	calculateDefenseGems($("#gems2").val());
 	calculateDefenseGems($("#gems3").val());
@@ -446,14 +449,29 @@ function recalculateDefenseGems()
 
 function recalculateSpeedGems()
 {
-	resetSpeedValues();
-	
 	calculateSpeedGems($("#gems1").val());
         calculateSpeedGems($("#gems2").val());
         calculateSpeedGems($("#gems3").val());
 	return;
 
 }
+
+function recalculateAssistGems()
+{
+	calculateAssistGems($("#gems1").val());
+	calculateAssistGems($("#gems2").val());
+	calculateAssistGems($("#gems3").val());
+
+}
+
+function recalculateVitalityGems()
+{
+        calculateVitalityGems($("#gems1").val());
+        calculateVitalityGems($("#gems2").val());
+        calculateVitalityGems($("#gems3").val());
+
+}
+
 /* these effect functions exist because gems have secondary effects on
 certain character features */
 
@@ -475,11 +493,23 @@ function calculateSpeedEffects(value)
 	{
 		$(".fwalkspeed").css("color", "red");
 		$(".bwalkspeed").css("color", "red");
+
+		$(".btotalframe").css("color", "red");
+                $(".bairframe").css("color", "red");
+                $(".bgroundframe").css("color", "red");
+                $(".binvulframe").css("color", "red");
+
 	}
 	else
 	{
 		$(".fwalkspeed").css("color", "green");
                 $(".bwalkspeed").css("color", "green");
+
+		$(".btotalframe").css("color", "green");
+                $(".bairframe").css("color", "green");
+                $(".bgroundframe").css("color", "green");
+                $(".binvulframe").css("color", "green");
+
 	}
 	/* convert value to decimal	
 	var binvulframe = parseFloat($(".binvulframe").html());
@@ -541,21 +571,112 @@ function calculateSpeedEffects(value)
 	$(".bgroundframe").html(bgroundframe);
 	$(".binvulframe").html(binvulframe);
 	
+	return;
+}
+
+
+function calculateDamageEffects(value)
+{
+	console.log("damage reduce: " + value);
+	
 	if (value < 0)
 	{
-		$(".btotalframe").css("color", "red");
-        	$(".bairframe").css("color", "red");
-        	$(".bgroundframe").css("color", "red");
-        	$(".binvulframe").css("color", "red");
+                value/=100;
+
+		$('.damage').css('color', 'red');
 	}
-	else
-	{
-                $(".btotalframe").css("color", "green");
-                $(".bairframe").css("color", "green");
-                $(".bgroundframe").css("color", "green");
-                $(".binvulframe").css("color", "green");
-	}	
-	return;
+        else
+        {	
+		value/=100;
+
+                $('.damage').css('color', 'green');
+        }
+	console.log("new reduce: " + value);
+	
+	$('.damage').each(function(){
+		console.log($(this).html());
+		
+		if ($(this).html() == "Damage")
+                {
+			$(this).css('color', 'black');
+                	return;
+                }
+		
+		if(($(this).is(":contains(',')" )))
+                {
+			var newString = "";
+                        var array = $(this).html().split(',');
+                        
+			for (var i = 0; i < array.length; ++i)
+                        {
+				console.log("Loop: " + array[i]);
+                                var newValue = ( parseFloat(array[i]) * value) + parseFloat(array[i]); 
+                                newString = newString + newValue + ", ";
+                      	}
+                        newString = newString.substring(0,newString.length-2);
+                        $(this).html(newString);
+                }
+                else
+                {
+			var newValue = parseFloat($(this).html());
+			newValue = (newValue * value) + newValue;
+			$(this).html(newValue);
+		}
+         })
+	
+}
+
+function calculateDefenseEffects(value)
+{
+        console.log("defense reduce: " + value);
+
+        if (value < 0)
+        {
+                value/=100;
+
+                $('.health').css('color', 'red');
+        }
+        else
+        {
+                value/=100;
+
+                $('.health').css('color', 'green');
+        }
+        console.log("new reduce: " + value);
+        $('.health').each(function(){
+                console.log($(this).html());
+
+                if ($(this).html() == "Health")
+                {
+                        $(this).css('color', 'black');
+                        return;
+                } 
+                else
+                {
+                        var newValue = parseFloat($(this).html());
+                        newValue = (newValue * value) + newValue;
+                        $(this).html(newValue);
+                }
+         })
+}
+
+function resetValuesWhileKeepingGemConfig()
+{
+	resetDamageValues();
+	resetSpeedValues();
+	resetDefenseValues();
+	resetAssistGemEffects();
+	resetVitalityGemEffects();
+}
+
+function resetAssistGemEffects()
+{
+	$("#additionalEffects").empty();
+}
+
+function resetVitalityGemEffects()
+{
+	$("#additionalEffects").empty();
 }
 function resetSpeedValues()
 {
