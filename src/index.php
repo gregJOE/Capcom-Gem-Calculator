849c196f3f8bec0fab8d52da48756089d86cf529
root <root@ubuntu.ubuntu-domain>
2012-07-02 16:27:34 -0400

    Added general attribute frame data to all characters. Things like health, forward and back dash numbers, etc. Also added main index page to the site.

---
+++ src/index.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title> Street Fighter X Tekken Gem Calculator </title>
+	</head>
+	
+	<body>
+		<h1> The Street Fighter X Tekken Gem Calculator </h1>
+		
+		<div id="about"> About </div>
+		<div id="faq"> FAQ </div>
+		<div id="list"> Characters
+			<div id="sfCharacters"> Street Fighter 
+				<ul> 
+					<li> <a href="characters/Abel"> Abel </a>
+					<li> <a href="characters/Akuma"> Akuma </a>
+					<li> <a href="characters/Balrog"> Balrog </a>
+					<li> <a href="characters/Cammy"> Cammy </a>
+					<li> <a href="characters/Dhalsim"> Dhalsim </a>
+					<li> <a href="characters/Guile"> Guile </a>
+					<li> <a href="characters/Ibuki"> Ibuki </a>
+					<li> <a href="characters/Juri"> Juri </a>
+					<li> <a href="characters/MBison"> MBison </a>
+					<li> <a href="characters/Poison"> Posion </a>
+					<li> <a href="characters/Ryu"> Ryu </a>
+					<li> <a href="characters/Sagat"> Sagat </a>
+					<li> <a href="characters/Vega"> Vega </a>
+					<li> <a href="characters/Zangief"> Zangief </a>
+				</ul> 
+			</div>
+			<div id="tkCharacters"> Tekken <ul> </ul> </div>
+		</div>
+	</body>
+</html>
