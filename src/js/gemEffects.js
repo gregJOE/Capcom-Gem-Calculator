0ef8a865eb915e08eee0d180b76195acbcaa3cc0
Gregory Job <job.gregory@gmail.com>
2012-12-04 21:47:35 -0500

    Moving functions around. Putting text manipulation functions inside its own JS file

--- src/js/gemEffects.js
+++ src/js/gemEffects.js
@@ -52,43 +52,6 @@ $('#gems3').live('change', function(){
 	calculateGemEffects(val);	
 });
 
-function createNewDamageString(string, gemValue)
-{
-	console.log(string);	
-	var regResult = string.match(regExpDamage);
-	if (regResult != null)
-	{
-		var newDamageString = "";
-		console.log(regResult);
-		
-		if (regResult.length > 1)
-		{
-			var newDamageString = calculateAndBuildMultiStringDamage(regResult, gemValue);
-			console.log(newDamageString);
-			
-			console.log(newDamageString.lastIndexOf(","))
-			console.log(newDamageString.length)
-			/* have this function trim if there's a comma */
-			if (newDamageString.lastIndexOf(",") == newDamageString.length-1)
-			{
-				newDamageString = newDamageString.substring(0,newDamageString.length-1);
-			}
-			return newDamageString;
-		}
-		else
-		{	
-			var newDamageString = calculateSingleStringDamage(regResult, gemValue);
-			console.log(newDamageString)
-			return newDamageString;
-		}
-	}
-	else
-	{
-		var newDamageString = calculateSingleStringDamage(regResult, gemValue);
-		return newDamageString;
-	}
-
-}
 /* DAMAGE GEM FUNCTIONS */
 function traverseDamageFields(gemTitle)
 {
@@ -180,66 +143,7 @@ function traverseDamageFields(gemTitle)
 
 /* USER CONFIG STUFF */
 
-function checkIfMultiplier(string)
-{
-	if (string.indexOf("x") != -1)
-	{
-		return true;
-	}
-	return false; 
-}
-
-function calculateSingleStringDamage(data, gemValue)
-{
-	var percentage = gemValue / 100.0
-	data = parseFloat(data);
-	return parseInt((data * percentage) + data)
-}
-
-function calculateAndBuildMultiStringDamage(data, gemValue)
-{
-	console.log(data);
-	var newString = ""
-	var isMultiplier = false;
-	for (var i = 0; i < data.length; i++)
-	{
-		console.log("char: " + data[i]);
-		if (data[i] == undefined)
-		{
-			break;
-		}
-		
-		if (isMultiplier)
-		{
-			newString = newString + data[i];
-			isMultiplier = false;
-			continue;
-		}
-		else
-		{
-			/* if checkIfMultiplier(data[i]) */
-			if (data[i].indexOf("x") != -1)
-			{
-				isMultiplier = true;
-				var value = calculateSingleStringDamage(data[i], gemValue)
-				newString = newString + value + "x";
-			}
-			else if (data[i].indexOf("/") != -1)
-			{
-				var value = calculateSingleStringDamage(data[i], gemValue)
-                                newString = newString + value + "/";
-			}	
-			else
-			{
-				var value = calculateSingleStringDamage(data[i], gemValue)
-				newString = newString + value + ",";
-			}
-		}
-	}
-	console.log("Result: " + newString);
-	return newString;
-}
-
+	
 function calculateGemEffects(gemEffect)
 {
 	resetValuesWhileKeepingGemConfig();
@@ -847,15 +751,14 @@ function returnDamageValueFromString(string, delminator)
 	/* note, this covers a small case. Must take into account multiple delimators. Use regular
 	expression? 
 	*/
-        else if (string.contains(","))
-        {
-                var numbers = []
-                var comma = $(this).html().indexOf(',', 0);
-                numbers.push($(this).html().substring(0, comma));
-                numbers.push($(this).html().substring(comma+1, length($(this).html())));
-                return numbers;
-        }
-
+	else if (string.contains(","))
+	{
+			var numbers = []
+			var comma = $(this).html().indexOf(',', 0);
+			numbers.push($(this).html().substring(0, comma));
+			numbers.push($(this).html().substring(comma+1, length($(this).html())));
+			return numbers;
+	}
 }
 
 /* Download / Upload stuff 
--- src/js/textManip.js
+++ src/js/textManip.js
@@ -1,5 +1,96 @@
-function parseText(text, deliminator)
+function createNewDamageString(string, gemValue)
 {
-	var splitString = text.split(deliminator);
-	return splitString;
+	console.log(string);	
+	var regResult = string.match(regExpDamage);
+	if (regResult != null)
+	{
+		var newDamageString = "";
+		console.log(regResult);
+		
+		if (regResult.length > 1)
+		{
+			var newDamageString = calculateAndBuildMultiStringDamage(regResult, gemValue);
+			console.log(newDamageString);
+			
+			console.log(newDamageString.lastIndexOf(","))
+			console.log(newDamageString.length)
+			/* have this function trim if there's a comma */
+			if (newDamageString.lastIndexOf(",") == newDamageString.length-1)
+			{
+				newDamageString = newDamageString.substring(0,newDamageString.length-1);
+			}
+			return newDamageString;
+		}
+		else
+		{	
+			var newDamageString = calculateSingleStringDamage(regResult, gemValue);
+			console.log(newDamageString)
+			return newDamageString;
+		}
+	}
+	else
+	{
+		var newDamageString = calculateSingleStringDamage(regResult, gemValue);
+		return newDamageString;
+	}
+}
+
+function checkIfMultiplier(string)
+{
+	if (string.indexOf("x") != -1)
+	{
+		return true;
+	}
+	return false; 
+}
+
+function calculateSingleStringDamage(data, gemValue)
+{
+	var percentage = gemValue / 100.0
+	data = parseFloat(data);
+	return parseInt((data * percentage) + data)
+}
+
+function calculateAndBuildMultiStringDamage(data, gemValue)
+{
+	console.log(data);
+	var newString = ""
+	var isMultiplier = false;
+	for (var i = 0; i < data.length; i++)
+	{
+		console.log("char: " + data[i]);
+		if (data[i] == undefined)
+		{
+			break;
+		}
+		
+		if (isMultiplier)
+		{
+			newString = newString + data[i];
+			isMultiplier = false;
+			continue;
+		}
+		else
+		{
+			/* if checkIfMultiplier(data[i]) */
+			if (data[i].indexOf("x") != -1)
+			{
+				isMultiplier = true;
+				var value = calculateSingleStringDamage(data[i], gemValue)
+				newString = newString + value + "x";
+			}
+			else if (data[i].indexOf("/") != -1)
+			{
+				var value = calculateSingleStringDamage(data[i], gemValue)
+                                newString = newString + value + "/";
+			}	
+			else
+			{
+				var value = calculateSingleStringDamage(data[i], gemValue)
+				newString = newString + value + ",";
+			}
+		}
+	}
+	console.log("Result: " + newString);
+	return newString;
 }
